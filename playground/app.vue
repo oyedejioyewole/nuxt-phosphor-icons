4661001ebae56dbc47d702677d541bae8c07fa41
OyewoleOyedeji <57059138+OyewoleOyedeji@users.noreply.github.com>
2024-01-01 10:56:52 +0100

    style(playground): Added custom theming

--- playground/app.vue
+++ playground/app.vue
@@ -1,6 +1,13 @@
 <script lang="ts" setup>
 import iconList from "#build/nuxt-phosphor-icons.mjs";
-import { pastelTheme } from "notivue";
+import { pastelTheme, type NotivueTheme } from "notivue";
+
+const theme: NotivueTheme = {
+  ...pastelTheme,
+  "--nv-success-accent": "#2d271d",
+  "--nv-success-bg": "#a5986a",
+  "--nv-success-fg": "#2d271d",
+};
 
 const icon = ref("");
 const { copy } = useClipboard();
@@ -38,7 +45,7 @@ async function copyToClipboard(text: string) {
 
 <template>
   <div
-    class="grid grid-cols-2 content-center min-h-screen mx-auto w-[90%] gap-x-10"
+    class="grid grid-rows-2 lg:grid-cols-2 content-center min-h-screen mx-auto w-[90%] gap-x-10"
   >
     <div class="space-y-10 flex flex-col justify-center">
       <h1 class="font-serif text-5xl text-copy-900">Playground</h1>
@@ -66,7 +73,10 @@ async function copyToClipboard(text: string) {
       </div>
     </div>
 
-    <div class="grid grid-cols-4 place-items-center" v-auto-animate>
+    <div
+      class="grid grid-cols-2 md:grid-cols-4 place-items-center"
+      v-auto-animate
+    >
       <button
         class="p-10 bg-accent-500 rounded-lg w-fit flex items-center"
         v-for="(icon, index) of displayedIcons"
@@ -92,6 +102,6 @@ async function copyToClipboard(text: string) {
   </div>
 
   <Notivue v-slot="item">
-    <Notifications :item="item" :theme="pastelTheme" />
+    <Notifications :item="item" :theme="theme" />
   </Notivue>
 </template>
