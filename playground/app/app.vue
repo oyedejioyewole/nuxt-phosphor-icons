c191d5303b78895382f993e07f91e99f51d97e85
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2025-08-28 18:32:26 +0100

    refactor: 🧹 Cleanup code in runtime component

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <PhosphorIcon name="acorn" />
+
+    <!-- All the keys should be preloaded even though only one is truthy -->
+    <PhosphorIcon :name="{ 'sun': true, 'moon-stars': false }" />
   </div>
 </template>
--- src/runtime/components/PhosphorIcon.vue
+++ src/runtime/components/PhosphorIcon.vue
@@ -1,41 +1,41 @@
 <script lang="ts" setup>
 import { computed, preloadComponents, resolveComponent } from '#imports'
-import type { PhosphorIconName } from '#phosphor-icons/types'
 import { getIconMap } from '../utils/icons'
 
 const iconMap = getIconMap()
 
+type PhosphorIconName = keyof typeof iconMap
+type PhosphorIconWeight = 'thin' | 'light' | 'regular' | 'bold' | 'fill' | 'duotone'
+
 interface PhosphorIconProps {
-  name: Record<PhosphorIconName, boolean> | PhosphorIconName
+  name: PhosphorIconName | Partial<Record<PhosphorIconName, boolean>>
   color?: string
   size?: string | number
-  weight?: 'thin' | 'light' | 'regular' | 'bold' | 'fill' | 'duotone'
+  weight?: PhosphorIconWeight
   mirrored?: boolean
 }
 
 const { name, ...options } = defineProps<PhosphorIconProps>()
-const normalizedName = computed((): PhosphorIconName => {
-  switch (typeof name) {
-    case 'object': {
-      const iconComponents = Object.keys(name).map(icon => iconMap[icon as PhosphorIconName])
-      preloadComponents(iconComponents)
+const normalizedName = computed(() => {
+  if (typeof name === 'string') return name
 
-      return Object.keys(name)
-        .find(key => name[key]) as PhosphorIconName
-    }
+  const iconEntries = Object.entries(name)
+  preloadComponents(iconEntries.map(([iconName]) => iconMap[iconName as PhosphorIconName]))
 
-    default:
-      return name
-  }
+  // Pick only the first truthy entry
+  const activeIcon = iconEntries.find(([, isTruthy]) => isTruthy)
+  return activeIcon ? activeIcon[0] : null
 })
 
 const Icon = computed(() => {
-  if (!Object.keys(iconMap).includes(normalizedName.value)) {
+  const iconMapEntry = Object.entries(iconMap).find(([nameFromMap]) => nameFromMap === normalizedName.value)
+
+  if (!iconMapEntry) {
     console.warn(`${normalizedName.value} icon doesn't exist, would use a blank <svg> element instead`)
     return 'svg'
   }
 
-  return resolveComponent(iconMap[normalizedName.value])
+  return resolveComponent(iconMapEntry[1])
 })
 </script>
 
