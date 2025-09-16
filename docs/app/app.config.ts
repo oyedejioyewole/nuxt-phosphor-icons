986477ecba1da7c99b6b464890399d5834672ca9
oyedejioyewole <57059138+oyedejioyewole@users.noreply.github.com>
2025-09-16 17:58:53 +0100

    docs: 🖊  Update `app.config` features and improve formatting

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -1,21 +1,22 @@
 export default defineAppConfig({
   pastelDocs: {
     features: [
-      'Nuxt 3 ready.',
+      'SSR ready.',
       'Typescript support.',
-      'Handy utility function for getting list of icons.',
+      'Performance focused.',
+      'Handy utility function for the icons.',
       'Full compatibility with components from @phosphor-icons/vue.',
     ],
     footer: {
       content: 'Made with :heart: by [oyedejioyewole](https://github.com/oyedejioyewole)',
       iconLinks: {
-        'github-logo': 'https://github.com/oyedejioyewole/nuxt-phosphor-icons',
-        'svgl:nuxt': 'https://nuxt.com'
-      }
+        'phosphor-logo': 'https://phosphoricons.com/',
+        'svgl:nuxt': 'https://nuxt.com',
+      },
     },
     headline: 'Phosphor Icons meets Nuxt',
     repo: 'oyedejioyewole/nuxt-phosphor-icons',
     themeColor: '#3c402b',
-    
+
   },
 })
